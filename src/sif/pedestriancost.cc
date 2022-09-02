4e50d21446aef4bf2baa97506edebc888c711c40
amaury.zarzelli <amaury.zarzelli@ign.fr>
2022-09-02 10:56:11 +0200

    Add exclude toll bridge tunnel to pedestrian

--- src/sif/pedestriancost.cc
+++ src/sif/pedestriancost.cc
@@ -634,7 +634,10 @@ bool PedestrianCost::Allowed(const baldr::DirectedEdge* edge,
       (!pred.deadend() && pred.opp_local_idx() == edge->localedgeidx() &&
        pred.mode() == TravelMode::kPedestrian) ||
       //      (edge->max_up_slope() > max_grade_ || edge->max_down_slope() > max_grade_) ||
-      ((pred.path_distance() + edge->length()) > max_distance_)) {
+      ((pred.path_distance() + edge->length()) > max_distance_) ||
+      (exclude_bridge_ && !pred.bridge() && edge->bridge()) ||
+      (exclude_tunnel_ && !pred.tunnel() && edge->tunnel()) ||
+      (exclude_toll_ && !pred.toll() && edge->toll())) {
     return false;
   }
 
@@ -669,7 +672,9 @@ bool PedestrianCost::AllowedReverse(const baldr::DirectedEdge* edge,
        pred.mode() == TravelMode::kPedestrian) ||
       //      (opp_edge->max_up_slope() > max_grade_ || opp_edge->max_down_slope() > max_grade_) ||
       opp_edge->use() == Use::kTransitConnection || opp_edge->use() == Use::kEgressConnection ||
-      opp_edge->use() == Use::kPlatformConnection) {
+      opp_edge->use() == Use::kPlatformConnection || (exclude_bridge_ && !pred.bridge() && edge->bridge()) ||
+      (exclude_tunnel_ && !pred.tunnel() && edge->tunnel()) ||
+      (exclude_toll_ && !pred.toll() && edge->toll())) {
     return false;
   }
 
